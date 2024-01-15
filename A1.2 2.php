b3f7b6fdeb986f6443a7aa3f4400a04f961ade63
Hugo Padilla El Alfa <hugoalfonsopadillajimenez@alumno.ieselrincon.es>
2024-01-15 09:18:34 +0000

    Subo el repositorio de Hugo

---
+++ A1.2 2.php
@@ -0,0 +1,15 @@
+<?php
+
+  // Array asociativo que contiene información sobre un estudiante: 
+  $estudiante = [
+    "nombre" => "Hugo Padilla",
+    "edad" => 20,
+    "dirección" => "Calle Barcelona 27"
+    ];
+
+  // Imprimir la información del estudiante:
+  echo "Nombre: " . $estudiante["nombre"] . "\n";
+  echo "Edad: " . $estudiante["edad"] . "\n";
+  echo "Dirección: " . $estudiante["dirección"] . "\n";
+
+?>
